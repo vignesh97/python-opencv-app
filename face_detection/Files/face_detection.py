422bfb6a9e3394ab36402bf63a6ec817f10c9264
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-19 12:31:11 -0400

    face detection using cv2.CascadeClassifier

---
+++ face_detection/Files/face_detection.py
@@ -0,0 +1,23 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+img = cv2.imread("photo.jpg")
+
+gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_img,scaleFactor=1.05, minNeighbors=5)
+
+
+
+    
+
+print(type(faces))
+
+print(faces)
+
+cv2.imshow("Gray",gray_img)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
