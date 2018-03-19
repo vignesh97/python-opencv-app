65aac08a5a93b3defd429a419d54a395774bb116
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-19 13:09:24 -0400

    Looping the frames and with exit key

--- videocapture/capture.py
+++ videocapture/capture.py
@@ -2,15 +2,17 @@ import cv2, time
 
 video = cv2.VideoCapture(0)
 
-check, frame = video.read()
 
-print(check)
 
-print(frame)
+a=1
+while True:
+    a=a+1
+    check, frame = video.read()
+    cv2.imshow("Capturing",frame)
+    key =cv2.waitKey(1)
+    if key==ord('q'):
+        break
 
-time.sleep(3)
-cv2.imshow("Capturing",frame)
-
-cv2.waitKey(0)
+print(a)
 video.release()
 cv2.destroyAllWindows()
