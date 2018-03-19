9b6f3013d57c826d5e01c4d87a34081fb008f7de
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-19 01:08:31 -0400

    resize image

--- read_image/script.py
+++ read_image/script.py
@@ -10,6 +10,10 @@ print(img.shape)
 
 print(img.ndim)
 
-cv2.imshow("Galaxy",img)
+
+resized_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+cv2.imshow("Galaxy",resized_image)
+cv2.imwrite("Galaxy_resized", resized_image)
+
 
 cv2.waitKey(0)
