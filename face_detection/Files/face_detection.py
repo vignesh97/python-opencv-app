ae8286ec61ac87bb3c4405aa09169d76373c1393
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-19 12:45:08 -0400

    tweaking scaleFactor and minNeighbors

--- face_detection/Files/face_detection.py
+++ face_detection/Files/face_detection.py
@@ -2,23 +2,19 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img = cv2.imread("photo.jpg")
+img = cv2.imread("news.jpg")
 
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray_img,scaleFactor=1.05, minNeighbors=5)
+faces = face_cascade.detectMultiScale(gray_img,scaleFactor=1.15, minNeighbors=3)
 
 for x,y,w,h in faces:
     img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
 
-
-
 print(type(faces))
-
 print(faces)
 
 resized = cv2.resize(img,(int(img.shape[1]/3),int(img.shape[0]/3)))
-
 
 cv2.imshow("Gray",gray_img)
 cv2.imshow("Resized",resized)
