944a9f09f35d027d5f3ebab8e358df6d70998fba
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-19 12:39:30 -0400

    face detection with rectange borders

--- face_detection/Files/face_detection.py
+++ face_detection/Files/face_detection.py
@@ -8,15 +8,20 @@ gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray_img,scaleFactor=1.05, minNeighbors=5)
 
+for x,y,w,h in faces:
+    img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
 
 
-    
 
 print(type(faces))
 
 print(faces)
 
+resized = cv2.resize(img,(int(img.shape[1]/3),int(img.shape[0]/3)))
+
+
 cv2.imshow("Gray",gray_img)
+cv2.imshow("Resized",resized)
 
 cv2.waitKey(0)
 
